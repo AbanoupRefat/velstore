78a39ce62e2722ab2e35ec20840ccf20d2185c93
AbanoupRefat <42110092.Abanoup@acu.edu.eg>
2025-12-06 08:03:37 +0200

    Add missing CSS files to Vite

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,20 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-                'resources/views/themes/xylo/sass/app.scss',
-                'resources/views/themes/xylo/js/app.js',
-                'resources/views/themes/xylo/css/animate.min.css',
-		'resources/views/themes/xylo/css/colors.css',
-                'resources/views/themes/xylo/css/slick.css',
-                'resources/views/themes/xylo/css/style.css',
-                'resources/views/themes/xylo/css/custom.css',
-                'resources/views/themes/xylo/js/main.js',
-                'resources/views/themes/xylo/js/slick.min.js',
-            ],
+    'resources/sass/app.scss',
+    'resources/js/app.js',
+    'resources/views/themes/xylo/sass/app.scss',
+    'resources/views/themes/xylo/js/app.js',
+    'resources/views/themes/xylo/css/animate.min.css',
+    'resources/views/themes/xylo/css/colors.css',
+    'resources/views/themes/xylo/css/quick-view.css',          // ADD THIS
+    'resources/views/themes/xylo/css/mobile-responsive.css',   // ADD THIS
+    'resources/views/themes/xylo/css/slick.css',
+    'resources/views/themes/xylo/css/style.css',
+    'resources/views/themes/xylo/css/custom.css',
+    'resources/views/themes/xylo/js/main.js',
+    'resources/views/themes/xylo/js/slick.min.js',
+],
             refresh: true,
         }),
     ],
